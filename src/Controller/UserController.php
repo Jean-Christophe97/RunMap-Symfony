6e2ff3fb05ee197c491cf29a8b0114e88911b19a
Jean-Christophe97 <juspinjean@gmail.com>
2020-01-07 14:12:11 +0100

    'u'

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -28,7 +28,7 @@ class UserController extends AbstractController
          // Encodage du mot de passe
         $user->setPassword($encoder->encodePassword($user, $user->getPassword()));
         //role user par défaut
-        $roles = array('ROLE_ADMIN');
+        $roles = array('ROLE_USER');
         $user->setRoles($roles);
         $entityManager = $this->getDoctrine()->getManager();
         $entityManager->persist($user);
